536d04a3268ab0017da510939b1027de62625343
Gerard Riera <gerardrp@ext.bershka.com>
2024-10-14 17:25:09 +0200

    Initial commit

---
+++ app-merge-dev/ContentView.swift
@@ -0,0 +1,20 @@
+//
+//  ContentView.swift
+//  app-merge-dev
+//
+//  Created by Gerard Riera Puig on 14/10/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
---
+++ app-merge-dev/app_merge_devApp.swift
@@ -0,0 +1,17 @@
+//
+//  app_merge_devApp.swift
+//  app-merge-dev
+//
+//  Created by Gerard Riera Puig on 14/10/24.
+//
+
+import SwiftUI
+
+@main
+struct app_merge_devApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
